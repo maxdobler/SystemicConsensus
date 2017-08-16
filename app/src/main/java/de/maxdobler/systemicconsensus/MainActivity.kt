1ef3cd75dc47730aa6cf142a49d9fde1353e8a74
maxdobler <max.dobler@posteo.de>
2017-08-16 21:07:38 +0200

    add toolbar to main actitvity

--- app/src/main/java/de/maxdobler/systemicconsensus/MainActivity.kt
+++ app/src/main/java/de/maxdobler/systemicconsensus/MainActivity.kt
@@ -1,17 +1,17 @@
 package de.maxdobler.systemicconsensus
 
-import android.arch.lifecycle.LifecycleActivity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import de.maxdobler.systemicconsensus.support.AppCompatLifecycleActivity
 import de.maxdobler.systemicconsensus.team.NewTeamActivity
 import de.maxdobler.systemicconsensus.team.TeamListRecyclerAdapter
 import de.maxdobler.systemicconsensus.team.TeamViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : LifecycleActivity() {
+class MainActivity : AppCompatLifecycleActivity() {
     private lateinit var viewModel: TeamViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +19,19 @@ class MainActivity : LifecycleActivity() {
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(TeamViewModel::class.java)
 
+        setSupportActionBar(toolbar)
+        setupNewTeamButton()
+        setupTeamList()
+    }
+
+    private fun setupNewTeamButton() {
         newTeamButton.setOnClickListener {
             val intent = Intent(this, NewTeamActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setupTeamList() {
         val teamsListRecyclerAdapter = TeamListRecyclerAdapter()
         teamList.layoutManager = LinearLayoutManager(this)
         teamList.adapter = teamsListRecyclerAdapter
---
+++ app/src/main/java/de/maxdobler/systemicconsensus/support/AppCompatLifecycleActivity.kt
@@ -0,0 +1,14 @@
+package de.maxdobler.systemicconsensus.support
+
+import android.arch.lifecycle.LifecycleRegistry
+import android.arch.lifecycle.LifecycleRegistryOwner
+import android.support.v7.app.AppCompatActivity
+
+
+abstract class AppCompatLifecycleActivity : AppCompatActivity(), LifecycleRegistryOwner {
+    private val mRegistry = LifecycleRegistry(this)
+
+    override fun getLifecycle(): LifecycleRegistry {
+        return mRegistry
+    }
+}
